3abf8e68ac36bd653c74d976679f6dfcc67205ac
Kelli Givens <kelligivens@live.co.za>
2018-09-17 01:20:09 -0400

    Allow course creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
