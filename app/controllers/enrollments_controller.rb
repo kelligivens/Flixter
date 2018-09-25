7bead7412fb1ec56676698026cb3d5f8f4db98a7
Kelli Givens <kelligivens@live.co.za>
2018-09-25 09:19:52 -0400

    Add student enrollment

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,6 +2,8 @@ class EnrollmentsController < ApplicationController
     before_action :authenticate_user!
 
   def create
+    current_user.enrollments.create(course: current_course)
+    redirect_to course_path(current_course)
   end
   
   private
