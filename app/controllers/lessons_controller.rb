9fd35adb897b7a33fedfafaf52631d1eb0d0c936
Kelli Givens <kelligivens@live.co.za>
2018-09-25 10:33:16 -0400

    Add permissions to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: :show
+  
   def show
+     @course = Course.find(params[:id])
+  end
+  
+  def current_course
+    @current_course ||= current_lesson.section.course
   end
   
   private
+  
+  def require_authorized_for_current_course
+    if current_course.user != current_user && ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You are not enrolled in this course'
+  end
+end
 
   helper_method :current_lesson
   def current_lesson
