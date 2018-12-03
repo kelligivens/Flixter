5eb58bdda36282b9bcefffcce5ce89d093d53733
kelligivens <kelligivens@live.co.za>
2018-12-03 12:38:53 -0500

    Added section drag and drop function

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -21,10 +21,14 @@ class Instructor::SectionsController < ApplicationController
     end
   end
   
-  helper_method :current_course
+ helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+       current_section.course
   end
+end
 
   def section_params
     params.require(:section).permit(:title)
