01480b3f28a1447e2e2104e72744c0112ea709e2
kelligivens <kelligivens@live.co.za>
2018-12-06 11:01:51 -0500

    Add new section form modal

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -15,8 +15,8 @@ class Instructor::CoursesController < ApplicationController
     end
   end
   
-   def show
-    @course = Course.find(params[:id])
+  def show
+    @section = Section.new
   end
   
   private
